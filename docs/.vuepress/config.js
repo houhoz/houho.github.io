73b98e085d892e4a1989ed649ecc892a48434569
yazhao.hou <yazhao.hou@upai.com>
2021-03-24 13:32:06 +0800

    feat: 修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-03-24 12:59:57
- * @LastEditTime: 2021-03-24 13:30:20
+ * @LastEditTime: 2021-03-24 13:31:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /myblog/docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
   base: '/',
-  dest: 'docs/.vuepress/dist',
+  dest: 'dist',
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
     navbar: [
