5a11dd350951536e217ba4e5a7e84dd0c4cbe287
yazhao.hou <yazhao.hou@upai.com>
2021-03-24 13:15:31 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-24 12:59:57
+ * @LastEditTime: 2021-03-24 13:04:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /myblog/docs/.vuepress/config.js
+ */
+module.exports = {
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+      {
+        text: 'Foo',
+        link: '/foo/',
+      },
+      {
+        text: 'Group',
+        children: ['/group/foo.md', '/group/bar.md'],
+      },
+      '/bar/README.md',
+    ],
+  },
+}
